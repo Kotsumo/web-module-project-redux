bea0f7faefc7d73d35812768e7ee3f6d365e05b6
David <davidellis1020@gmail.com>
2021-03-17 17:30:54 -0700

    Completed project

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { AddFeature } from '../actions/carSalesActions';
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={() => props.AddFeature(props.feature.id)} className="button">
+        Add
+      </button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(null, {AddFeature})(AdditionalFeature);
